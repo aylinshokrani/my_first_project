d96a746c09d73e2f4e00dc5dfd08e03e97e95293
aylinshokrani <aylin.shokrani8080@gmail.com>
2025-11-07 15:36:03 +0330

    Add files via upload

---
+++ محاسبه معدل.py
@@ -0,0 +1,67 @@
+﻿
+def calculate(grades):
+    total = sum(grades)
+    count = len(grades)
+    average = total / count
+    return average
+
+n = int(input("enter the number of courses: "))
+grades = []
+
+for i in range(n):
+    grade = float(input(f"enter grade {i+1}: "))
+    grades.append(grade)
+
+
+average = calculate(grades)
+
+print(f"Average: {average:.2f}")
+
+
+if average >= 17:
+    print("great!")
+elif average >= 10:
+    print("pass")
+else:
+    print("fail")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
